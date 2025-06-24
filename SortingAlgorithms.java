db17b45f2c55b6213105a078271cce07ab9af582
sushma-ramesh <112920367+sushma-ramesh@users.noreply.github.com>
2025-06-24 11:18:30 +0530

    Add files via upload

---
+++ SortingAlgorithms.java
@@ -0,0 +1,53 @@
+public class SortingAlgorithms {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {5, 2, 8, 1, 3};
+        int[] arr2 = {7, 4, 6, 2, 9};
+
+        System.out.println("Bubble Sort:");
+        bubbleSort(arr1);
+        printArray(arr1);
+
+        System.out.println("Selection Sort:");
+        selectionSort(arr2);
+        printArray(arr2);
+    }
+}
